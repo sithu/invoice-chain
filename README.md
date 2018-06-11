1d14bebce6ba14c31a4ef87d35cdf6d84bb44a68
Sithu Aung <sithu_aung@intuit.com>
2018-06-11 16:15:53 -0700

    fix qbchain CLI

--- README.md
+++ README.md
@@ -4,6 +4,57 @@
 
 ```sh
 $ cd cmd
-$ go get github.intuit.com/payments/qbchain
+$ go get github.intuit.com/payments/qbchain.git
 $ go build -o qbchain
 ```
+
+# Usage
+
+## Starting a node
+
+You can start as many nodes as you want with the following command
+
+`./qbchain -port=<port-number>`
+
+
+## Endpoints
+
+
+### Requesting the Blockchain of a node
+
+* `GET 127.0.0.1:8000/chain`
+
+### Mining some coins
+
+* `GET 127.0.0.1:8000/mine`
+
+### Adding a new transaction
+
+* `POST 127.0.0.1:8000/transactions/new`
+
+* __Body__: A transaction to be added
+
+  ```json
+  {
+    "sender": "sender-address-te33412uywq89234g",
+    "recipient": "recipient-address-j3h45jk23hjk543gf",
+    "amount": 1000
+  }
+  ```
+
+### Register a new node in the network
+Currently you must add each new node to each running node.
+
+* `POST 127.0.0.1:8000/nodes/register`
+
+* __Body__: A list of nodes to add
+
+  ```json
+  {
+     "nodes": ["http://127.0.0.1:8001", <more-nodes>]
+  }
+  ```
+
+### Resolving Blockchain differences in each node
+
+* `GET 127.0.0.1:8000/nodes/resolve`
--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.intuit.com/payments/qbchain"
-    "log"
-    "net/http"
-    "strings"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.intuit.com/payments/qbchain.git"
 )
 
 func main() {
-    serverPort := flag.String("port", "8000", "http port number where server will run")
-    flag.Parse()
+	serverPort := flag.String("port", "8000", "http port number where server will run")
+	flag.Parse()
 
-    blockchain := qbchain.NewBlockchain()
-    nodeID := strings.Replace(qbchain.PseudoUUID(), "-", "", -1)
+	blockchain := qbchain.NewBlockchain()
+	nodeID := strings.Replace(qbchain.PseudoUUID(), "-", "", -1)
 
-    log.Printf("Starting gochain HTTP Server. Listening at port %q", *serverPort)
+	log.Printf("Starting QB Chain HTTP Server. Listening at port %q", *serverPort)
 
-    http.Handle("/", gochain.NewHandler(blockchain, nodeID))
-    http.ListenAndServe(fmt.Sprintf(":%s", *serverPort), nil)
+	http.Handle("/", qbchain.NewHandler(blockchain, nodeID))
+	http.ListenAndServe(fmt.Sprintf(":%s", *serverPort), nil)
 }
